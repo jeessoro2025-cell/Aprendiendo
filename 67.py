ac953e61dd5acb46e6adb7188bfa1c3500e5c3e0
Bluey <jeessoro2024@gmail.com>
2026-06-20 11:53:10 -0700

    Modifique el .py con un proyecto pequeño de 23 lineas

--- 67.py
+++ 67.py
@@ -1,4 +1,22 @@
-h = "Hola, GitHub!"
-o = 67
+Nombre = input("Ingrese su nombre: ")
 
-print(f"{o} Para ti: " + h)
+print("Hola " + Nombre + ", bienvenido a Python")
+
+Edad = input("Ingrese su edad: ")
+
+print("Tienes " + Edad + " Años")
+
+Color = input("Cual es tu color favorito? ")
+
+print("El color " + Color + " es hermoso")
+
+Hola = input("Que te gustaria que hiciera por ti? ")
+
+if Hola == "Ya me quiero ir":
+    print("¡Hasta pronto!")
+elif Hola == "Son im crine":
+    print("Sonion")
+else:
+    print("El 67 es el mejor numero del mundo")
+    
+    
